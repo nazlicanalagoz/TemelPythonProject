45a4e7b3bd7f0c92d08e9191cb7d11543b06541a
Nazlıcan Alagöz <80030123+nazlicanalagoz@users.noreply.github.com>
2022-10-12 21:12:01 +0300

    Update and rename proje.py to flatten.py

--- flatten.py
+++ flatten.py
@@ -1,6 +1,4 @@
-"""Temel Python Project
-
-### Flatten
+""" Flatten
  Bir listeyi düzleştiren (flatten) fonksiyon yazın. Elemanları birden çok katmanlı listelerden ([[3],2] gibi) oluşabileceği gibi, non-scalar verilerden de oluşabilir. Örnek olarak:
 
 input: [[1,'a',['cat'],2],[[[3]],'dog'],4,5]
